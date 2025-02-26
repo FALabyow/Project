bec9d2228fb1fe6e6147348c8efd005d78647dca
FALabyow <ruhaynaadje08@gmail.com>
2025-02-26 21:30:23 +0800

    Added Interfaces and DTOs for Products

--- Project.Application/DTOs/ProductDTO.cs
+++ Project.Application/DTOs/ProductDTO.cs
@@ -4,16 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Project.Domain.Entities
+namespace Project.Application.DTOs
 {
-    public class Product
+    public class ProductDTO
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
-        public int Stock {  get; set; }
+        public int Stock { get; set; }
         public Guid CategoryId { get; set; }
-        public Category Category { get; set; }
-
     }
 }
---
+++ Project.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Project.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Application.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(Guid id);
+    }
+}
