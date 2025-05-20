1f529ebd63a2439e10271ecd34c9c72c7871d7dd
FALabyow <ruhaynaadje08@gmail.com>
2025-05-20 16:42:42 +0800

    Updated Code

--- Project.API/Program.cs
+++ Project.API/Program.cs
@@ -40,20 +40,20 @@ namespace Project.API
            
             builder.Services.AddSwaggerGen();
 
-            //var listener = new TcpListener(IPAddress.Loopback, 0);
-            //listener.Start();
-            //int port = ((IPEndPoint)listener.LocalEndpoint).Port;
-            //listener.Stop();
+            var listener = new TcpListener(IPAddress.Loopback, 0);
+            listener.Start();
+            int port = ((IPEndPoint)listener.LocalEndpoint).Port;
+            listener.Stop();
 
-            //// Log and write port to file
-            //string portFilePath = @"C:\Temp\port.txt";
-            //Directory.CreateDirectory(@"C:\Temp");
-            //File.WriteAllText(portFilePath, port.ToString());
+            // Log and write port to file
+            string portFilePath = @"C:\Temp\port.txt";
+            Directory.CreateDirectory(@"C:\Temp");
+            File.WriteAllText(portFilePath, port.ToString());
 
-            //builder.WebHost.ConfigureKestrel(options =>
-            //{
-            //    options.Listen(IPAddress.Loopback, port);
-            //});
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.Listen(IPAddress.Loopback, port);
+            });
 
             var app = builder.Build();
 
--- ProjectForm/Program.cs
+++ ProjectForm/Program.cs
@@ -10,34 +10,34 @@ namespace ProjectForm
         static void Main()
         {
             string apiPath = Path.Combine(Application.StartupPath, "ProjectAPI", "Project.API.exe");
-            //try
-            //{
-            //    if (File.Exists(apiPath))
-            //    {
-            //        webApiProcess = Process.Start(new ProcessStartInfo
-            //        {
-            //            FileName = apiPath, // Use full path
-            //            WorkingDirectory = Path.GetDirectoryName(apiPath)!,
-            //            CreateNoWindow = true,
-            //            UseShellExecute = false,
-            //        });
+            try
+            {
+                if (File.Exists(apiPath))
+                {
+                    webApiProcess = Process.Start(new ProcessStartInfo
+                    {
+                        FileName = apiPath, // Use full path
+                        WorkingDirectory = Path.GetDirectoryName(apiPath)!,
+                        CreateNoWindow = true,
+                        UseShellExecute = false,
+                    });
 
-            //        Application.ApplicationExit += (s, e) =>
-            //        {
-            //            try { if (webApiProcess is { HasExited: false }) webApiProcess.Kill(); }
-            //            catch { }
-            //        };
-            //    }
+                    Application.ApplicationExit += (s, e) =>
+                    {
+                        try { if (webApiProcess is { HasExited: false }) webApiProcess.Kill(); }
+                        catch { }
+                    };
+                }
 
-            //    else
-            //    {
-            //        MessageBox.Show("Web API executable not found!" + apiPath);
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show("Error starting Web API: " + ex.Message + apiPath);
-            //}
+                else
+                {
+                    MessageBox.Show("Web API executable not found!" + apiPath);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error starting Web API: " + ex.Message + apiPath);
+            }
 
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
